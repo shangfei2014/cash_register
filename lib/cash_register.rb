bf550bbf68946530bc20d27b295836148aef88ec
shang.fei <shang.fei@rea-group.com>
2016-03-23 15:24:15 +0800

    format price output

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,9 +11,9 @@ class CashRegister
     receipt = ""
     receipt << "***<没钱赚商店>购物清单***\n"
     item_list.each do |item|
-      receipt << "名称：#{item.name}，数量：#{item.count}#{item.unit}，单价：#{item.price_per_unit.round(2)}(元)，小计：#{item.price}(元)\n"
+      receipt << "名称：#{item.name}，数量：#{item.count}#{item.unit}，单价：#{'%.2f' % item.price_per_unit}(元)，小计：#{'%.2f' % item.price}(元)\n"
     end
-    receipt << "----------------------\n总计：#{calculate(item_list)}(元)\n**********************\n"
+    receipt << "----------------------\n总计：#{'%.2f' % calculate(item_list)}(元)\n**********************\n"
     receipt
   end
 
